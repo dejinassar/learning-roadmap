115c21e7f2194f7656b8cf66652c313086e3aa48
dejinassar <dejinassar@gmail.com>
2025-03-10 15:16:34 +0100

    Monday week 1:Python Basics

---
+++ Phase 1/Week 1/Monday/loops.py
@@ -0,0 +1,25 @@
+#Looping through a list via for loop
+fruits = ["apple", "banana", "cherry"]
+for fruit in fruits:
+    print(fruit)
+
+#Using the range() function
+for i in range(5):  # Loops from 0 to 4
+  print(i)
+
+
+for i in range(1, 11):
+   print(i)
+   
+#Print Even Numbers from 1 to 20 Using while Loop
+num = 2
+while num <= 20:
+    print(num)
+    num += 2
+
+#Sum of Numbers from 1 to 10
+total = 0
+for i in range(1, 10):
+    total += i
+print("Sum:", total)
+
